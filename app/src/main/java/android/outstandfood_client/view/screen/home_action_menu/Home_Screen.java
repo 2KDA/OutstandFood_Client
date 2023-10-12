7e05d9671808d75ebf0c8646b9a2f656370fd610
duyanhkieu20092003@gmail.com <100137620+2KDA@users.noreply.github.com>
2023-10-12 14:50:00 +0700

    menu bottom

---
+++ app/src/main/java/android/outstandfood_client/view/screen/home_action_menu/Home_Screen.java
@@ -0,0 +1,23 @@
+package android.outstandfood_client.view.screen.home_action_menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.outstandfood_client.R;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Home_Screen extends AppCompatActivity {
+
+    private BottomNavigationView btnNavigation_home_ActionMenu_Main_layout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+
+
+
+    }
+}

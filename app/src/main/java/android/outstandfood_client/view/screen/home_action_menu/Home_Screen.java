d9f4f1f4267a8c39e0b9dd1355eb57555e664872
duyanhkieu20092003@gmail.com <100137620+2KDA@users.noreply.github.com>
2023-10-17 21:07:56 +0700

    fix

--- app/src/main/java/android/outstandfood_client/view/screen/home_action_menu/Home_Screen.java
+++ app/src/main/java/android/outstandfood_client/view/screen/home_action_menu/Home_Screen.java
@@ -8,7 +8,6 @@ import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 import android.outstandfood_client.R;
 import android.outstandfood_client.databinding.ActivityHomeScreenBinding;
-import android.outstandfood_client.databinding.FragmentHomeBinding;
 import android.outstandfood_client.view.screen.fragment.CartFragment;
 import android.outstandfood_client.view.screen.fragment.HistoryFragment;
 import android.outstandfood_client.view.screen.fragment.HomeFragment;
@@ -30,15 +29,13 @@ public class Home_Screen extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int id = item.getItemId();
                 if (id == R.id.home_menu_Home) {
-
+                    replaceFragment(new HomeFragment());
                 } else if (id == R.id.home_menu_Orders) {
                     replaceFragment(new CartFragment());
                 } else if (id == R.id.home_menu_Wallet) {
                     replaceFragment(new HistoryFragment());
                 } else if (id == R.id.home_menu_Profile) {
                     replaceFragment(new ProfileFragment());
-                } else if (id == R.id.home_menu_Home) {
-                    replaceFragment(new HomeFragment());
                 }
                 return true;
             }

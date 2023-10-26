ceeb8913db668b82b94c4a102ccefc128b57aaf2
Hạnh(Badboy) <tieuconghanh34@gmail.com>
2023-10-26 22:02:31 +0700

    xong đăng nhập đăng kí

--- app/src/main/java/android/outstandfood_client/view/screen/home_action_menu/Home_Screen.java
+++ app/src/main/java/android/outstandfood_client/view/screen/home_action_menu/Home_Screen.java
@@ -2,21 +2,34 @@ package android.outstandfood_client.view.screen.home_action_menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.outstandfood_client.R;
+import android.outstandfood_client.models.User;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class Home_Screen extends AppCompatActivity {
 
     private BottomNavigationView btnNavigation_home_ActionMenu_Main_layout;
-
+    User user;
+    private String userId;
+    private String username;
+    private String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-
+        Intent intent = getIntent();
+        if (intent != null && intent.hasExtra("userData")) {
+            user = intent.getParcelableExtra("userData");
+            if (user != null) {
+                username = user.getUsername();
+                password = user.getPassword();
+                userId = user.get_id();
+            }
+        }
 
 
     }

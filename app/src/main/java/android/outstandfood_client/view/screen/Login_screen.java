c5e26af98a527363ae782c07cc4a85deff186895
Hạnh(Badboy) <117272856+TieuCongHanh@users.noreply.github.com>
2023-11-30 19:53:57 +0700

    up

--- app/src/main/java/android/outstandfood_client/view/screen/Login_screen.java
+++ app/src/main/java/android/outstandfood_client/view/screen/Login_screen.java
@@ -1,5 +1,7 @@
 package android.outstandfood_client.view.screen;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.outstandfood_client.R;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -52,7 +54,7 @@ public class Login_screen extends AppCompatActivity {
 
         username = findViewById(R.id.edt_Login_User);
         password = findViewById(R.id.edt_Login_Password);
-        btn_dangnhap = findViewById(R.id.btn_Login);
+        btn_dangnhap = findViewById(R.id.btnLoginEmail);
         textViewSignUp = findViewById(R.id.textViewSignUp);
         btn_dangnhap.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,94 +100,6 @@ public class Login_screen extends AppCompatActivity {
 
                 } else {
 
-
-                    http.setRequestProperty("Content-Type", "application/json");
-                    //Tạo đối tượng out dữ liệu ra khỏi ứng dụng để gửi lên server
-                    OutputStream outputStream = http.getOutputStream();
-                    BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
-                    bufferedWriter.append(jsonObject.toString());
-                    //Xóa bộ đệm
-                    bufferedWriter.flush();
-                    bufferedWriter.close();
-                    outputStream.close();
-                    //Nhận lại dữ liệu phản hồi
-                    int responseCode = http.getResponseCode();
-
-                    if (responseCode == HttpURLConnection.HTTP_OK) {
-                        // Xử lý dữ liệu phản hồi từ server
-                        InputStream inputStream = http.getInputStream();
-                        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                        StringBuilder response = new StringBuilder();
-                        String line;
-                        while ((line = bufferedReader.readLine()) != null) {
-                            response.append(line);
-                        }
-                        bufferedReader.close();
-                        inputStream.close();
-
-                        // Giải mã dữ liệu JSON phản hồi từ server để lấy thông tin người dùng
-                        JSONObject responseJson = new JSONObject(response.toString());
-                        String userId = responseJson.optString("userId");
-                        String returnedUsername = responseJson.optString("username");
-                        String returnedPassword = responseJson.optString("password");
-                        String returnedRole= responseJson.optString("role");
-                        String returnedemailUser= responseJson.optString("emailUser");
-                        String returnedFullname= responseJson.optString("name");
-                        String returnedimage= responseJson.optString("image");
-                        String returnedphone= responseJson.optString("phone");
-                        boolean returnedisActive= responseJson.getBoolean("isActive");
-
-
-                        // Tạo đối tượng UserData để truyền sang màn hình Home
-                        User user = new User(returnedUsername, returnedPassword, userId,
-                                returnedFullname,returnedRole,returnedemailUser,returnedimage,returnedphone,returnedisActive);
-
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(Login_screen.this, "Chào mừng " + returnedUsername + " đã đến với thế giới đồ ăn", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(Login_screen.this, Home_Screen.class);
-                                intent.putExtra("userData", user);
-                                startActivity(intent);
-                                finish(); // Đóng màn hình đăng nhập
-                            }
-                        });
-                    } else if (responseCode == HttpURLConnection.HTTP_BAD_REQUEST ) {
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(Login_screen.this, "Thông tin tài khoản có sự nhầm lẫn", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }else if (responseCode == HttpURLConnection.HTTP_UNAUTHORIZED ) {
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(Login_screen.this, "Thông tin mật khẩu có sự nhầm lẫn", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    } else if (responseCode == HttpURLConnection.HTTP_FORBIDDEN) {
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(Login_screen.this, "Tài khoản của bạn đã bị khóa", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    } else {
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                Toast.makeText(Login_screen.this, "Đăng nhập không thành công", Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                    }
-
-                } catch (MalformedURLException e) {
-                    throw new RuntimeException(e);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
 
                 }
             }

0ab53733153ad8d82ed736001e66689ff2ece3d0
Hạnh(Badboy) <117272856+TieuCongHanh@users.noreply.github.com>
2023-12-14 19:20:00 +0700

    update tiếng việt check user

--- app/src/main/java/android/outstandfood_client/view/screen/adapter/ListProductAdapter.java
+++ app/src/main/java/android/outstandfood_client/view/screen/adapter/ListProductAdapter.java
@@ -1,12 +1,19 @@
 package android.outstandfood_client.view.screen.adapter;
 
+import static java.security.AccessController.getContext;
+
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.outstandfood_client.R;
 import android.outstandfood_client.databinding.ItemCategoryBinding;
 import android.outstandfood_client.databinding.ItemProductBinding;
 import android.outstandfood_client.interfaces.FoodInterface;
 import android.outstandfood_client.models.Product;
+import android.outstandfood_client.object.CommonActivity;
+import android.outstandfood_client.object.SharedPrefsManager;
+import android.outstandfood_client.view.screen.Login_screen;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -44,15 +51,28 @@ public class ListProductAdapter extends RecyclerView.Adapter<ListProductAdapter.
     @SuppressLint("SetTextI18n")
     @Override
     public void onBindViewHolder(@NonNull viewHolderCategory holder, int position) {
-        Product product=list.get(position);
+        Product product = list.get(position);
         Glide.with(mContext).load(product.getImage()).error(R.drawable.avartar).into(holder.binding.imFood);
 //        holder.binding.tvPrice.setText("Số lượng: "+String.valueOf(product.getQuantity()));
-        holder.binding.tvPriceNum.setText(product.getPrice()+" VNĐ");
+        holder.binding.tvPriceNum.setText(product.getPrice() + " VNĐ");
         holder.binding.tvName.setText(product.getName());
         holder.binding.getRoot().setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-             foodInterface.addFood(product);
+
+                if (SharedPrefsManager.getUser(mContext) != null) {
+                    foodInterface.addFood(product);
+                } else {
+                    CommonActivity.createDialog((Activity) mContext,
+                            "Bạn phải đăng nhập mới có thể sử dụng chức năng của cửa hàng !",
+                            "Outstand'Food", "Hủy", "Đồng ý",
+                            null,
+                            v -> {
+                                Intent intent = new Intent(mContext, Login_screen.class);
+                                mContext.startActivity(intent);
+                            }
+                    ).show();
+                }
             }
         });
     }
